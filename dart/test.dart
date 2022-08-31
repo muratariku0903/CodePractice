7fdc501cf4c0736c2ecb0bf6678766e93fb9f999
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-01 00:08:18 +0900

    update test

--- dart/test.dart
+++ dart/test.dart
@@ -3,10 +3,10 @@ import './solution.dart';
 import './testCases.dart';
 
 class Test {
-  void check(Solution solution, TestCase tc) {
-    test('Test', () {
+  void check(Solution solution, TestCase tc, String message) {
+    test(message, () {
       final nowMs = DateTime.now().millisecondsSinceEpoch;
-      final res = solution.solve(tc.nums, tc.diff);
+      final res = solution.solve(tc.n);
       final elapsedTime = DateTime.now().millisecondsSinceEpoch - nowMs;
       expect(res, equals(tc.ans));
       print('Elapsed Time : ' + elapsedTime.toString() + ' ms');
