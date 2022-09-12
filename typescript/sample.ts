7d18d52611ec2b87eab3604bff90352c00fa31c4
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-13 00:26:57 +0900

    test validation of ajv package

--- typescript/sample.ts
+++ typescript/sample.ts
@@ -9,55 +9,51 @@ import { counter } from './functions/counter';
 import { chdir, exit, memoryUsage, nextTick } from 'process';
 import { tests } from './test';
 import { MinBinaryHeap, MaxBinaryHeap } from './classes/heap';
+import Ajv, { JSONSchemaType } from 'ajv';
 
-class Tmp {
-    private name;
 
-    constructor(name?: string) {
-        this.name = name;
-    }
+const ajv = new Ajv();
 
-    public tmp() {
-        if (!this.name) ;
-        console.log(this.name);
-    }
+type RegisterInfo = {
+    contractNumber: string;
+    name: string;
+    nameKana: string;
+    postcode: number;
+    password: string;
+    address: string;
 }
 
-const tmp = new Tmp();
-tmp.tmp();
-
-
-
-exit();
-
-
-// 返り値もジェネリクスを使って、指定できるようにすれば
-const func = (head: ListNode | null): boolean => {
-    const swap = (node: ListNode | null): ListNode | null => {
-        let prev = null, tmp: ListNode | null;
-        while (node) {
-            tmp = node.next;
-            node.next = prev;
-            prev = node;
-            node = tmp;
-        }
-        return prev;
-    }
-
-    console.log(getArrFromListNode(swap(head)));
-
-    return true;
+const schema: JSONSchemaType<RegisterInfo> = {
+    type: 'object',
+    required: ['contractNumber', 'name', 'nameKana', 'postcode', 'password', 'address'],
+    properties: {
+        contractNumber: { type: 'string', pattern: '^[0-9]{20}$' },
+        name: { type: 'string', minLength: 1, pattern: '^[^\x20-\x7e]*$', not: { pattern: '^\u3000*$' } },
+        nameKana: { type: 'string', minLength: 1, pattern: '^[\u3000ァ-ンヴー]*$', not: { pattern: '^\u3000*$' } },
+        postcode: { type: 'integer', minimum: 1000000, maximum: 9999999 },
+        password: { type: 'string', pattern: '^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{8,32}$' },
+        address: { type: 'string', pattern: '^[^\x20-\x7e]*$', nullable: true },
+    },
+    additionalProperties: false,
 }
 
+const info: RegisterInfo = {
+    contractNumber: '11111111111111111111',
+    name: '　村田',
+    nameKana: 'ムラタ　',
+    postcode: 1000000,
+    password: 'AAAAaaa1a',
+    address: '千葉県　',
+}
 
-const test_data = new TestData();
-const test_cases: (ListNode | null)[][] = [
-    // [createListNode([1, 2, 2, 1])],
-    // [createListNode([1, 2, 2, 1, 3])],
-    // [createListNode([1, 2, 2, 2, 1])],
-    // [createListNode([1, 2, 5, 2, 1])],
-    [createListNode([1, 2, 5, 6, 7, 5, 2, 1])],
-    [createListNode([1, 2, 5, 6, 7, 5, 2])],
-];
+const validate = ajv.compile(schema);
 
-tests['list_node'](func, test_cases);
+const res = validate(info);
+
+console.log(validate.errors);
+
+console.log(res);
+
+
+
+
