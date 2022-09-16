05ebdebb64a3328ebea00941c79301afa2581ccc
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-17 02:01:22 +0900

    update list_node class

--- dart/classes/list_node.dart
+++ dart/classes/list_node.dart
@@ -3,6 +3,12 @@ class ListNode {
   ListNode? next;
   ListNode([this.val = 0, this.next]);
 
+  static ListNode? createListNode(List<int> nums) {
+    return nums.isEmpty
+        ? null
+        : ListNode(nums[0], createListNode(shiftNums(nums)));
+  }
+
   static List<int> getListFromListNode(ListNode? node) {
     if (node == null) return [];
     return [node.val] + getListFromListNode(node.next);
@@ -14,5 +20,10 @@ class ListNode {
     if (node1 != null && node2 == null) return false;
     if (node1!.val != node2!.val) return false;
     return isSameListNode(node1.next, node2.next);
+  }
+
+  static List<int> shiftNums(List<int> nums) {
+    nums.removeAt(0);
+    return nums;
   }
 }
