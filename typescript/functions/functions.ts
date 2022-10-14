8c5bb37382ee82058d89bffea63e2eed20dc1db7
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-10-15 07:32:40 +0900

    add new function

--- typescript/functions/functions.ts
+++ typescript/functions/functions.ts
@@ -14,3 +14,16 @@ export function fact(num: bigint): bigint {
     if (num <= 1) return BigInt(1);
     return (num * (fact(BigInt(num) - BigInt(1)))) % BigInt(1000000007);
 }
+
+export const bs = (nums: number[], n: number): number => {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = ~~((left + right) / 2);
+        if (nums[mid] === n) return mid;
+        if (nums[mid] < n) left = mid + 1;
+        if (nums[mid] > n) right = mid - 1;
+    }
+
+    return -1;
+}
